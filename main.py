263fdc421bbfdc13b056ba152492a5eaadbd28be
dabaozhai <89297153+dabaozhai@users.noreply.github.com>
2023-02-09 21:31:39 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import re
 import os
+import requests
 
-if os.path.exists("aa.txt"):
-    os.remove("aa.txt")
+if os.path.exists("tv_list.txt"):
+    os.remove("tv_list.txt")
 
-with open("aa.txt", "w") as f:
+with open("tv_list.txt", "w") as f:
     url = "https://agit.ai/guot54/ygbh/raw/branch/master/zB/zB.txt"
     content = requests.get(url, timeout=5).content.decode("utf-8")
     content = re.sub("_\d+,#", ",#", content)
